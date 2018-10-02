1007d8c6994e9c0d71db25d52e1628c2666d1be7
maryP96 <43380908+maryP96@users.noreply.github.com>
2018-10-02 16:17:36 +0200

    forma

--- sketch.js
+++ sketch.js
@@ -4,8 +4,83 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(40);
+  angleMode(DEGREES);
+
+  frameRate(12);
 }
 
 function draw() {
   // put drawing code here
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*300);
+  noFill();
+
+  stroke(lerpColor(color('#0fefca'),color('#ea0043'), frameCount/120));
+  stroke(30,0,0,50);
+
+    line(200,200,cos(frameCount*3)*200,sin(frameCount*3)*200);
+  pop();
+
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*30);
+  noFill();
+  //stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/150));
+  stroke(200)
+  ellipse(20,0,300,200);
+  pop();
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*30);
+  noFill();
+  //stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/150));
+  stroke(100)
+  ellipse(20,0,200,300);
+  pop();
+
+  // push();
+  // translate(width/2,height/2);
+  // rotate(frameCount*30);
+  // noFill();
+  // stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/120));
+  // line(30,0,cos(frameCount*3)*100,sin(frameCount*3)*100);
+  // pop();
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*3);
+  noFill();
+  stroke(lerpColor(color('#fffff7'), color(' #1d0000'), frameCount/120));
+  line(cos(frameCount*30)*300,sin(frameCount*30)*330,cos(frameCount*30)*300,sin(frameCount*30)*300);
+  pop();
+
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*3);
+  noFill();
+  //stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/150));
+  stroke(300)
+  ellipse(180,0,cos(frameCount*3)*20,20);
+  pop();
+
+  push();
+  translate(width/2,height/2);
+  rotate(frameCount*3);
+  noFill();
+  stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/120));
+  line(0,0,cos(frameCount*3)*100,sin(frameCount*3)*100);
+  pop();
+
+
+  // Stops the draw function if a certain condition avaluates true
+  if (frameCount == 120) {
+    noLoop();
+  }
 }
